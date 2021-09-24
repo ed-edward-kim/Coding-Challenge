51aee973aaf31546b349b5b50421df95b48ccc85
Edward Kim <edki7145@colorado.edu>
2021-09-23 19:27:13 -0600

    Got Express connected with React

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000; //Express server running on port 5000, while React runs on 3000
+
+const http = require('http'); //might not need
+
+console.log("Starting server!")
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/express_backend', (req, res) => { 
+    res.send({ express: 'Express backend is connected to react!' }); 
+});
+
+// const server = app.listen(process.env.PORT || 5000, () => {
+//     console.log(`Express running → PORT ${server.address().port}`);
+// });
+app.listen(port, () => console.log(`Listening on port ${port}`));
